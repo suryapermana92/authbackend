26bfec639d22ab44289e1bd042ec91771458437a
Surya Permana <suryapermana92@gmail.com>
2017-12-08 04:53:25 +0700

    commit

--- functions/routes/request_otp.js
+++ functions/routes/request_otp.js
@@ -1,6 +1,48 @@
 const admin = require('firebase-admin');
 const axios = require('axios')
+const FormData = require('form-data')
 // const client = require('./../twilio-helper');
+
+let sendMesabotRequest = (phone, code) => {
+    const body = {
+        "destination": phone,
+        "text": "Your Code Is: " + code,
+        // Uncomment line di bawah untuk penggunaan fitur masking
+        // "masking" => "NAMA_MASKING",
+    }
+
+    const headers = {
+        "client-id": "YXpRKz1F",
+        "client-secret": "QZEhXDhS",
+        "content-type": "application/json"
+    }
+
+    return axios({
+        method: "POST",
+        url: "https://mesabot.com/api/v2/send",
+        data: body,
+        headers: headers
+    })
+}
+
+let sendTwillioRequest = (phone, code) => {
+    let data = new FormData();
+
+    let destinationPhoneNumber = '+62' + phone.substr(1, phone.length - 1);
+
+    data.append('From', '+12243850972');
+    data.append('To', destinationPhoneNumber);
+    data.append('Body', `Your OTP code is ${code}`)
+
+    let headers = Object.assign({}, data.getHeaders(), {'authorization': 'Basic QUNlMTFhYWE3MjQyOWE4MDliZjI4NTcwMjJjMzZmNTNhMjpiNTY0MDg0NjZkMjljNjA5NjIyZGRhNWM4OGMxNmEwZg=='})
+
+    return axios({
+        method: "POST",
+        url: "https://api.twilio.com/2010-04-01/Accounts/ACe11aaa72429a809bf2857022c36f53a2/Messages.json",
+        headers: headers,
+        data: data
+    })
+}
 
 module.exports = (req, res) => {
 
@@ -13,74 +55,18 @@ module.exports = (req, res) => {
     }
     
     //sanitizing
-    const phone = String(req.body.phone)
-                    .replace(/[^\d]/g,'')
+    const phone = String(req.body.phone).replace(/[^\d]/g,'')
 
-    admin.auth().createUser({ uid: phone })
-    .then(user => {
-        const angka = Math.random() * 8999 + 1000;
-        const code = Math.floor(angka);
-        res.send({ phone, code })
+    admin.auth().getUser(phone)
+        .then((userRecord) => {
+            
+            // TODO
+            // Create Code
+            const angka = Math.random() * 8999 + 1000;
+            const code = Math.floor(angka);
 
-        const body = {
-            "destination": phone,
-            "text": "Your Code Is: "+ code,
-            // Uncomment line di bawah untuk penggunaan fitur masking
-            // "masking" => "NAMA_MASKING",
-          }
-        const headers = {
-            "client-id": "YXpRKz1F",
-            "client-secret": "QZEhXDhS",
-            "content-type": "application/json"
-          }
-        axios({
-            method: "POST",
-            url: "https://mesabot.com/api/v2/send",
-            data: body,
-            headers: headers
-        })
-            .then(() => {
-            admin.database().ref('users/' + phone)
-                .update({ code: code, valid: true })
-                .then(() => {
-                return res.send({ message: 'code has been sent' });
-                })
-                .catch((error) => {
-                    return res.send({ message: 'gagal save database' })
-                }) ;
-    });
-})
-
-    
-    .catch((err) => {
-        if (err) {
-            admin.auth().getUser(phone)
-            .then((userRecord) => {
-                
-                // TODO
-                // Create Code
-                const angka = Math.random() * 8999 + 1000;
-                const code = Math.floor(angka);
-                res.send({ phone, code })
-
-                const body = {
-                    "destination": phone,
-                    "text": "Your Code Is: "+ code,
-                    // Uncomment line di bawah untuk penggunaan fitur masking
-                    // "masking" => "NAMA_MASKING",
-                  }
-                const headers = {
-                    "client-id": "YXpRKz1F",
-                    "client-secret": "QZEhXDhS",
-                    "content-type": "application/json"
-                  }
-                axios({
-                    method: "POST",
-                    url: "https://mesabot.com/api/v2/send",
-                    data: body,
-                    headers: headers
-                })
-                    .then(() => {
+            sendTwillioRequest(phone, code)
+                .then((r) => {
                     admin.database().ref('users/' + phone)
                         .update({ code: code, valid: true })
                         .then(() => {
@@ -89,11 +75,31 @@ module.exports = (req, res) => {
                         .catch((error) => {
                             return res.send({ message: 'gagal save database' })
                         });
+                });
+        })
+        .catch((err) => {
+            console.log(err)
+            // Assume there is no user yet
+            admin.auth().createUser({ uid: phone })
+                .then(user => {
+                    const angka = Math.random() * 8999 + 1000;
+                    const code = Math.floor(angka);
+        
+                    sendTwillioRequest(phone, code)
+                        .then((r) => {
+                            
+                            admin.database().ref('users/' + phone)
+                                .update({ code: code, valid: true })
+                                .then(() => {
+                                    return res.send({ message: 'code has been sent' });
+                                })
+                                .catch((error) => {
+                                    return res.send({ message: 'gagal save database' })
+                                });
                     });
-                })
-            };
-        });
-    }           
+            })
+        })
+}         
                     
                     
 // admin.auth().getUser(phone)
--- functions/routes/verify_otp.js
+++ functions/routes/verify_otp.js
@@ -15,37 +15,37 @@ module.exports = (req, res) => {
     // const code = parseInt(String(req.body.code).replace(/[^\d]/g, ''))
     // res.send(phone)
 
-    // admin.auth().getUser(phone)
-    //     .then(() => {
-    //         res.send(otp, phone)
-    //         //todo baca code dari database sesuai nomor telp
-    //         const ref = admin.database().ref('users/' + phone)
+    admin.auth().getUser(phone)
+        .then(() => {
+            // res.send({otp, phone})
+            //todo baca code dari database sesuai nomor telp
+            const ref = admin.database().ref('users/' + phone)
 
-    //         ref.on('value', (snapshot) => {
-    //             ref.off();
+            ref.on('value', (snapshot) => {
+                ref.off();
 
-    //             const userData = snapshot.val();
+                const userData = snapshot.val();
 
-    //             if (userData.code !== otp) {
-    //                 return res.status(422).send({
-    //                     message: 'Code does not match'
-    //                 })
-    //             }
-    //             if (!userData.valid) {
-    //                 return res.status(422).send({
-    //                     message: 'This code has been used'
-    //                 })
-    //             }
-    //             ref.update({ valid: false });
+                if (String(userData.code) !== otp) {
+                    return res.send({
+                        message: 'Code does not match'
+                    })
+                }
+                if (!userData.valid) {
+                    return res.status(402).send({
+                        message: 'This code has been used'
+                    })
+                }
+                ref.update({ valid: false });
                 
-    admin.auth().createCustomToken(phone)
-    .then((token) => {
-        res.send({ token: token })
-    });
-    //         })
-    //     })
-    //     .catch((error) => {
-    //         return res.status(422).send(error)
-        // })
+                admin.auth().createCustomToken(phone)
+                .then((token) => {
+                    res.send({ token: token })
+                });
+            })
+        })
+        .catch((error) => {
+            return res.status(403).send(error)
+        })
             
 };
